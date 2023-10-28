30ec2f56a0babe2913ba168fedfbd5de0ae6779e
unknown <solankikiran43300@gmail.com>
2023-10-28 20:01:14 +0530

    added voice

--- client/src/components/chat/chat.tsx
+++ client/src/components/chat/chat.tsx
@@ -1,26 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Typewriter from 'typewriter-effect';
 import socketIOClient from 'socket.io-client';
 import "./styles.css";
 
-const initialMessages = [
+enum MessageType {
+  Text,
+  Audio
+}
+
+interface Body {
+  type: MessageType,
+  data: string
+}
+
+interface ChatMessage {
+  author: string,
+  body: Body,
+  timeout: number
+}
+
+const initialMessages: ChatMessage[] = [
   {
     author: 'bot',
-    body: 'Hello',
+    body: { type: MessageType.Text, data: 'Hello' },
     timeout: 800
   }
 ];
 
-const Message = ({ data }) => {
+const Message = ({ data }: { data: ChatMessage }) => {
   const { author, body } = data;
-  console.log(data)
+  console.log(data);
   let finalBody;
 
-  if (Array.isArray(body)) {
+  if (Array.isArray(body)) {    
     finalBody = body.map((item, index) => (
-      <>
+      <React.Fragment key={index}>
         {item === "..." && (
-          <div class="typing">
+          <div className="typing">
             <span></span>
             <span></span>
             <span></span>
@@ -31,32 +47,55 @@ const Message = ({ data }) => {
             {item.text}
           </a>
         )}
-      </>
-
+      </React.Fragment>
     ));
   } else {
-    if(body === "...") {
-      finalBody = <div class="c-chat__message typing"><span></span><span></span><span></span></div>
-    } else {
-      if(author === "bot") {
-        finalBody = <div className="c-chat__message"><Typewriter options={{ strings: body, autoStart: true,delay: 5, loop: false }} /> </div>;
+    if (body.type === MessageType.Text) {
+      if (body.data === "...") {
+        finalBody = (
+          <div className="c-chat__message typing">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        );
       } else {
-        finalBody = <div className="c-chat__message"> {body} </div>;
+        if (author === "bot") {
+          finalBody = (
+            <div className="c-chat__message">
+              <Typewriter
+                options={{
+                  strings: body.data,
+                  autoStart: true,
+                  delay: 5,
+                  loop: false,
+                }}
+              />
+            </div>
+          );
+        } else {
+          finalBody = <div className="c-chat__message"> {body.data} </div>;
+        }
       }
+    } else {
+      finalBody = <div><audio controls>
+        <source src={body.data} type="audio/wav" />
+        Your browser does not support the audio element.
+      </audio></div>;
     }
-    
   }
 
   return (
-    <li className={`c-chat__item c-chat__item--${author}`}>
-      {finalBody}
-    </li>
+    <li className={`c-chat__item c-chat__item--${author}`}>{finalBody}</li>
   );
 };
 
 const Chat = () => {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
   const [questionSubmitted, setQuestionSubmitted] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordedAudio, setRecordedAudio] = useState(null);
+  const mediaRecorder = useRef(null);
 
   const socket = socketIOClient('http://localhost:5000/chat');
 
@@ -64,7 +103,11 @@ const Chat = () => {
     start();
     socket.on('response', (response) => {
       setQuestionSubmitted(false);
-      updateMessage({ author: 'bot', body: response, timeout: 1000 });
+      console.log(response);
+      updateMessage({ author: 'bot', body: {
+        type: MessageType.Text,
+        data: response
+      }, timeout: 1000 });
     });
 
     // Clean up the socket connection when the component unmounts
@@ -85,34 +128,41 @@ const Chat = () => {
     }, 700);
   };
 
-  const addMessage = (item) => {
+  const addMessage = (item: ChatMessage) => {
     setMessages((prevMessages) => [...prevMessages, item]);
     const items = document.querySelectorAll('li');
     const lastItem = items[items.length - 1];
     document.querySelector('.c-chat__list').scrollTop = lastItem?.offsetTop + lastItem?.clientHeight;
   };
 
-  const updateMessage = (item) => {
+  const updateMessage = (item: ChatMessage) => {
     setMessages((prevMessages) => {
       const arr = [...prevMessages];
-      arr[prevMessages.length - 1] = item
-      return arr
+      arr[prevMessages.length - 1] = item;
+      return arr;
     });
     const items = document.querySelectorAll('li');
     const lastItem = items[items.length - 1];
     document.querySelector('.c-chat__list').scrollTop = lastItem?.offsetTop + lastItem?.clientHeight;
   };
 
-  const sendMessage = (message) => {
+  const sendMessage = (message: Body) => {
     if (questionSubmitted) {
       // Do nothing if a question has already been submitted
       return;
     }
 
     setQuestionSubmitted(true);
-    addMessage({ author: 'bot', body: "...", timeout: 1000 });
+    addMessage({ author: 'bot', body: {
+      type: MessageType.Text,
+      data: "..."
+    }, timeout: 1000 });
     setTimeout(() => {
-      socket.emit("message",message);
+      if (message.type == MessageType.Text) {
+        socket.emit("message", message.data);
+      } else {
+        socket.emit("voice", message.data);
+      }
     }, 2000); // 2 seconds delay before sending the message
   };
 
@@ -122,12 +172,94 @@ const Chat = () => {
 
     addMessage({
       author: 'human',
-      body: input.value,
+      body: {
+        type: MessageType.Text,
+        data: input.value
+      },
+      timeout: 1000
     });
 
-    sendMessage(input.value);
+    sendMessage({
+      type: MessageType.Text,
+      data: input.value
+    });
 
     e.target.reset();
+  };
+
+  const sendVoiceToBackend = (x: any) => {
+
+  }
+
+  const startRecording = () => {
+    if (isRecording) {
+      // If already recording, stop the recording
+      stopRecording();
+    } else {
+      // If not recording, start recording
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          console.log("HERE NOW")
+          mediaRecorder.current = new MediaRecorder(stream);
+          const audioChunks = [];
+
+          mediaRecorder.current.ondataavailable = (e) => {
+            if (e.data.size > 0) {
+              audioChunks.push(e.data);
+            }
+          };
+
+          mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+
+            // Convert audioBlob to base64
+            const reader = new FileReader();
+            reader.onload = (event) => {
+              const base64Audio = event.target.result;
+              console.log("NOW UPDATING")
+              console.log(base64Audio);
+              console.log(typeof (base64Audio));
+
+              setRecordedAudio(base64Audio);
+              addMessage({
+                author: "human",
+                timeout: 1000,
+                body: {
+                  type: MessageType.Audio,
+                  data: base64Audio as string
+                }
+              })
+
+              sendMessage({
+                type: MessageType.Audio,
+                data: base64Audio as string
+              })
+
+              setIsRecording(false);
+              // Send the audioBlob or base64Audio to your backend API
+              // sendVoiceToBackend(base64Audio);
+              
+            };
+
+            reader.readAsDataURL(audioBlob);
+          };
+
+          mediaRecorder.current.start();
+          setIsRecording(true);
+        })
+        .catch((error) => {
+          console.error('Error accessing the microphone:', error);
+        });
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+      mediaRecorder.current.stop();
+      console.log(mediaRecorder)
+      setIsRecording(!isRecording);
+    }
   };
 
   let cssClass = ['c-chat'];
@@ -139,10 +271,7 @@ const Chat = () => {
   return (
     <div className={cssClass.join(' ')}>
       <ul className="c-chat__list">
-        {messages.map((message, index) => (
-
-          <Message key={index} data={message} />
-        ))}
+        {messages.map((message, index) => <Message key={index} data={message} />)}
       </ul>
       <form className="c-chat__form" onSubmit={handleSubmit}>
         <input
@@ -155,6 +284,9 @@ const Chat = () => {
           disabled={questionSubmitted}
         />
       </form>
+      <button className='recorder' onClick={startRecording}>
+        {isRecording ? 'Stop Voice Recording' : 'Start Voice Recording'}
+      </button>
     </div>
   );
 };
